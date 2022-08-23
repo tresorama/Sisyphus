ff15ede11329be1c7a4a3b0c0cfeb8f9a2b9d3dd
katzman <steve.r.katzman@gmail.com>
2022-08-23 21:47:59 +0000

    Added better claimable/reset logic, switched to a typechain project managed using yarn

--- contracts/Sisyphus.sol
+++ contracts/Sisyphus.sol
@@ -3,91 +3,131 @@ pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "./libraries/ABDKMath64x64.sol";
 
 contract Sisyphus is Ownable, ReentrancyGuard{
 
+    using ABDKMath64x64 for *;
+
     // GAME PARAMETERS 
     address public currentWinner;
-    uint256 public timerDuration = 10; // 24 hours, 3600 s/hr 
-    // uint256 public timerDuration = 24*3600; // 24 hours, 3600 s/hr 
-    uint256 public lastPush = 0; // Parameter for recording timestamp of each boulder push
-    uint256 public expirationTime = 0; // Parameter for setting the new expiration time
-    uint256 public startingPrice = 0.005 ether; // start the price at 0.005 ether each round
-    uint256 public currentPrice = startingPrice;
-    address[] currentParticipants; // list of current participants
+    uint256 public currentPrice;
+    uint256 lastPushTime; // Parameter for recording timestamp of each boulder push
+    uint256 expirationTime; // Parameter for setting the new expiration time
+    uint256 pushCount;
+    uint256 gameCount;
+    mapping(uint256 => mapping(address => uint256[])) pushNumByPlayerGame; // Record when a player has pushed for each game
 
-    // ECONOMIC PARAMETERS 
-    uint256 public percentRateIncrease = 100; // 1% increase for each successive push
-    uint256 public percentToBoulder = 3000; // 30% of push value goes to the Boulder (prize pool)
-    uint256 public percentToPushers = 6900; // 69% of push value goes to previous pushers 
-    uint256 public percentToReserve = 100; // 1% of push value goes to platform profit
-    uint256 percentBasis = 10000; 
+    struct GameParams {
+        uint256 startingPrice;
+        uint256 timerDuration;
+        uint256 percentRateIncrease; // increase for each successive push
+        uint256 percentToBoulder; // push value goes to the Boulder (prize pool)
+        uint256 percentToPushers; // push value goes to previous pushers 
+        uint256 percentToReserve; // push value goes to platform profit
+        uint256 lastPush;
+    }
+    mapping(uint256 => GameParams) GameParamsByGame;
+    GameParams public PendingParams; // Owner can set new game params while a game is running - will take effect when the next game starts 
+
     uint256 public boulder = 0; // Value of the boulder
     uint256 public reserve = 0; 
-    
-    
-    mapping(address => uint256) public pendingWithdrawals;
+    uint256 constant percentBasis = 10000; 
 
     constructor() {
-        expirationTime = block.timestamp + timerDuration;
+        gameCount++;
+        GameParamsByGame[gameCount].startingPrice = 0.005 ether; // start the price at 0.005 ether each round
+        GameParamsByGame[gameCount].timerDuration = 24 hours;
+        GameParamsByGame[gameCount].percentRateIncrease = 100;  // 1% increase for each successive push
+        GameParamsByGame[gameCount].percentToBoulder = 3000;    // 30% of push value goes to the Boulder (prize pool)
+        GameParamsByGame[gameCount].percentToPushers = 6900;    // 69% of push value goes to previous pushers 
+        GameParamsByGame[gameCount].percentToReserve = 100;     // 1% of push value goes to platform profit
+        PendingParams = GameParamsByGame[gameCount];            // Init defaults
+        
+        // Init params for first game
+        expirationTime = block.timestamp + GameParamsByGame[gameCount].timerDuration;
+        currentPrice = GameParamsByGame[gameCount].startingPrice;
+                
     }
 
-    function pushTheBoulder() external payable nonReentrant {
+    function pushTheBoulder() external payable {
         require(msg.value == currentPrice,
             "Must push boulder with exactly push price"
         );
+        require(expirationTime < block.timestamp,
+            "The game needs to be reset by the contract operator");
 
-        // Logic is slightly different for fist push since there's no one to pay yield to 
-        if(boulder == 0){
-            currentWinner = msg.sender;
-        }
-        // Handle nominal case where we have a yield to pay out to each previous pusher 
-        else {
-            address previousWinner = currentWinner;
-            currentWinner = msg.sender;
-
-            // Record previous owner into participant list
-            currentParticipants.push(previousWinner);
-            
-            // Determine how many participants there are
-            uint256 len = currentParticipants.length;
-            
-            // Determine what the cut per participant should be
-            uint256 cut = _getCutPer(msg.value);
-
-            // Add the value of the cut to each particpant's withdraw balance
-            for(uint i=0; i<len; i++) {
-                address participant = currentParticipants[i];
-                pendingWithdrawals[participant] += cut;
-            }
-        }
+        // Record push
+        pushCount++;
+        pushNumByPlayerGame[gameCount][msg.sender].push(pushCount);
+        currentWinner = msg.sender;
 
         // Increment boulder value and take some for the reserves 
-        boulder += _getBoulderAdd(msg.value);  
-        reserve += _getReserveTake(msg.value);
+        boulder += _getBoulderAdd(gameCount, msg.value);  
+        reserve += _getReserveTake(gameCount, msg.value);
 
         // Set up parameters for next push
-        currentPrice = _getNewPrice();
-        lastPush = block.timestamp; 
-        expirationTime = lastPush + timerDuration;
-        
+        currentPrice = _getNewPrice(gameCount);
+        lastPushTime = block.timestamp; 
+        expirationTime = lastPushTime + GameParamsByGame[gameCount].timerDuration;
     }
 
-    // To ensure that the game is fair and doesn't charge insane gas to any participant, the platform will reset the game after each expiration
-    function resetTheBoulder() external onlyOwner {
-        require(block.timestamp > expirationTime, "Timer has not expired"); // Ensures game validity; no rug pulls here, sir 
-        pendingWithdrawals[currentWinner] += boulder;
+    // Anyone can call this when the time is appropriate
+    function resetTheBoulder() external {
+        require(block.timestamp > expirationTime, "Timer has not expired"); 
+        GameParamsByGame[gameCount].lastPush = pushCount;
+        pushCount = 0;
         boulder = 0;
-        delete currentParticipants;
-        currentWinner = msg.sender;
+        gameCount++;
+        GameParamsByGame[gameCount] = PendingParams;
+        expirationTime = lastPushTime + GameParamsByGame[gameCount].timerDuration;
+        currentWinner = address(0);
     }
     
     // Allows any previous user to withdraw their accrued balance from the platform
-    function withdrawDeposit() external nonReentrant {
-        uint256 withdrawal = pendingWithdrawals[msg.sender];
-        require(withdrawal > 0, "Nothing to withdraw");
-        pendingWithdrawals[msg.sender] = 0;
-        payable(msg.sender).transfer(withdrawal);
+    function claimWinningsByGame(uint256 _game) external nonReentrant {
+        uint256[] memory pushes = pushNumByPlayerGame[_game][msg.sender];
+        require(pushes.length > 0, "Nothing to withdraw");
+        require(gameCount > _game, "Game is not over yet");
+        uint256 val;
+        for (uint256 i; i == 0; i++) {
+            uint256 pushNum = pushes[i];
+            val += _valuePerPush(_game, pushNum);
+        }
+        delete pushNumByPlayerGame[_game][msg.sender];
+        (bool success, ) = msg.sender.call{value:val}("");
+        require(success, "Failed to transfer winnings");
+    }
+
+    function _valuePerPush(uint256 _game, uint256 _push) internal view returns (uint256) {
+        uint256 lastPush = GameParamsByGame[_game].lastPush;
+        return _calcValue(_game, lastPush, _push);
+    }
+
+    function _calcValue(uint256 _game, uint256 _lastPush, uint256 _push) internal view returns (uint256) {
+        if(_push < _lastPush) {
+            return _calcValue(_game, _lastPush, _push++) + _valueForPusher(_game, _push);
+        }
+        return _valueForPusher(_game, _push); // Last push does not get a split
+    }
+
+    function _valueForPusher(uint256 _game, uint256 _push) internal view returns (uint256) {
+        uint256 r = GameParamsByGame[_game].percentRateIncrease;
+        uint256 x0 = GameParamsByGame[_game].startingPrice;
+        int128 x = 1 + ABDKMath64x64.divu(r,percentBasis);
+        return ABDKMath64x64.mulu(ABDKMath64x64.pow(x,_push),x0);
+    }
+
+    function _getBoulderAdd(uint256 _game, uint256 value) internal view returns (uint256) {
+        return (value *  GameParamsByGame[_game].percentToBoulder) / percentBasis; 
+    }
+
+    function _getReserveTake(uint256 _game, uint256 value) internal view returns (uint256) {
+        return (value * GameParamsByGame[_game].percentToReserve) / percentBasis; 
+    }
+
+    function _getNewPrice(uint256 _game) internal view returns (uint256) {
+        return (currentPrice * GameParamsByGame[_game].percentRateIncrease) / percentBasis;
     }
 
     // Allows the platform to withdraw accrued revenue (mostly to pay for gas resetting the game)
@@ -97,20 +137,7 @@ contract Sisyphus is Ownable, ReentrancyGuard{
             reserve -= amount;
     }
 
-    function _getBoulderAdd(uint256 value) internal view returns (uint256) {
-        return (value *  percentToBoulder) / percentBasis; 
-    }
-
-    function _getCutPer(uint256 value) internal view returns (uint256) {
-        uint256 len = currentParticipants.length;
-        return (value *  percentToPushers) / (percentBasis * len);
-    }
-
-    function _getReserveTake(uint256 value) internal view returns (uint256) {
-        return (value * percentToReserve) / percentBasis; 
-    }
-
-    function _getNewPrice() internal view returns (uint256) {
-        return (currentPrice * percentRateIncrease) / percentBasis;
+    function changeGameParams(GameParams calldata NewParams) external onlyOwner {
+        PendingParams = NewParams; 
     }
 }
